f0291d8d83b970018deec5592dd5051d5a0ee83c
anh.dn <anh.dn@citigo.net>
2017-04-08 09:45:58 +0700

    refactor(KVAmountInputTextField): recode AmountTextField
    support 3 type of number: Percentage, Currency, Quantity.

--- LEAmountInputView/KVAmountInputTextField.h
+++ LEAmountInputView/KVAmountInputTextField.h
@@ -8,33 +8,72 @@
 
 #import <UIKit/UIKit.h>
 
-typedef void (^KVAmountInputTextFieldValueChanged)(double amountValue, BOOL shouldShowDotKey);
+typedef enum : NSUInteger {
+    KVAmountInputTextFieldTypePercentage,
+    KVAmountInputTextFieldTypeCurrency,
+    KVAmountInputTextFieldTypeQuantity,
+} KVAmountInputTextFieldType;
+
+@class KVAmountInputTextField;
+
+/**
+ *  The LEAmountInputTextFieldDelegate protocol defines the amount messages sent to a text field delegate. All of the methods of this protocol are optional.
+ *
+ *  Inherits from UITextFieldDelegate.
+ */
+@protocol KVAmountInputTextFieldDelegate <UITextFieldDelegate>
+
+@optional
+
+/**
+ *  Asks the delegate whether the amount should be changed.
+ *
+ *  If you do not implement this method, the default return value is YES.
+ *
+ *  @param textField The text field object that is asking whether the amount should change.
+ *  @param amount    The new amount.
+ *
+ *  @return YES if the amount should be changed or NO if it should not.
+ */
+- (BOOL)textField:(KVAmountInputTextField *)textField shouldChangeAmount:(NSNumber *)amount;
+
+/**
+ *  Tells the delegate that the amount changed. It does not call this method when you programmatically set the amount.
+ *
+ *  If you do not implement this method, the default return value is depend on the `type`.
+ *  @param textField The text field object that is notifying you of the change.
+ *  @param amount    The amount that was changed.
+ */
+- (void)textField:(KVAmountInputTextField *)textField didChangeAmount:(NSNumber *)amount;
+
+@end
 
 @interface KVAmountInputTextField : UITextField
 
-/*!
- * Option show dotkey or double zero key in numpad.
- */
-@property (nonatomic, assign) BOOL shouldShowDotKey;
+@property (nonatomic, assign) KVAmountInputTextFieldType type;
+
+@property (nonatomic, weak) id<KVAmountInputTextFieldDelegate> delegate;
 
 /**
  *  The amount value of the `KVAmountInputTextField`.
+ *  This value is nulable depend on type.
  */
-@property (nonatomic, assign) double amountValue;
+@property (nonatomic, strong) NSNumber *amount;
 
 /*!
  * The number formater of the `KVAmountInputTextField` text.
+ * Default is format for currency number
  */
-@property (nonatomic, strong) NSNumberFormatter *numberFormater;
+@property (nonatomic, strong) NSNumberFormatter *numberFormatter;
 
 /**
- *  No value display text
+ *  No value display text, orrcu clear textfield
  */
-@property (nonatomic, strong) NSString *noValueDisplayText;
+@property (nonatomic, assign) BOOL resetToZeroIfCleared;
 
-/*!
- * The block return amountValue
- */
-@property (nonatomic, copy) KVAmountInputTextFieldValueChanged valueChangedBlock;
+- (instancetype)initWithFrame:(CGRect)frame andInputType:(KVAmountInputTextFieldType)type;
+- (instancetype)initWithCoder:(NSCoder *)aDecoder andInputType:(KVAmountInputTextFieldType)type;
+
+
 
 @end
--- LEAmountInputView/KVAmountInputTextField.m
+++ LEAmountInputView/KVAmountInputTextField.m
@@ -8,8 +8,11 @@
 
 #import <FontAwesomeKit/FontAwesomeKit.h>
 
-#import "LEAmountInputView.h"
+#import "LENumberPad.h"
 #import "KVAmountInputTextField.h"
+
+#define NA_BACKSPACE_BUTTON_INDEX 11
+#define NA_DOT_OR_THOUDSAND_BUTTON_INDEX 9
 
 @interface KVAmountInputTextField()<LENumberPadDataSource, LENumberPadDelegate, UITextFieldDelegate>
 
@@ -20,10 +23,28 @@
 
 @implementation KVAmountInputTextField
 
+@synthesize delegate;
+
+- (instancetype)initWithFrame:(CGRect)frame andInputType:(KVAmountInputTextFieldType)type {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setUpViewWithInputType:type];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder andInputType:(KVAmountInputTextFieldType)type {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self setUpViewWithInputType:type];
+    }
+    return self;
+}
+
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        [self setUpView];
+        [self setUpViewWithInputType:KVAmountInputTextFieldTypeCurrency];
     }
     return self;
 }
@@ -31,49 +52,33 @@
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        [self setUpView];
+        [self setUpViewWithInputType:KVAmountInputTextFieldTypeCurrency];
     }
     return self;
 }
 
-- (void)setUpView {
+- (void)setUpViewWithInputType:(KVAmountInputTextFieldType)type {
     self.inputView = self.numberPad;
-    self.text = [self.numberFormater stringFromNumber:@(_amountValue)];
-    self.noValueDisplayText = @"0";
-    self.delegate = self;
-}
-
-- (BOOL)textFieldShouldClear:(UITextField *)textField {
-    if ([self.delegate isEqual:self]) {
-        self.amountValue = 0;
-        if (self.valueChangedBlock) {
-            self.valueChangedBlock(0, self.shouldShowDotKey);
-        }
-        return NO;
-    } else {
-        _amountValue = 0;
-        if ([self.delegate respondsToSelector:@selector(textFieldShouldClear:)]) {
-            return [self.delegate textFieldShouldClear:self];
-        }
-        return YES;
-    }
-}
-
-- (void)onClearButtonTapped:(UIButton *)sender {
-    self.amountValue = 0;
-    self.text = self.noValueDisplayText;
-    if (self.valueChangedBlock) {
-        self.valueChangedBlock(0, self.shouldShowDotKey);
-    }
+    self.text = [self.numberFormatter stringFromNumber:self.amount];
+    self.type = type;
+    super.delegate = self;
 }
 
 - (NSString *)buttonTitleAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.item == 11) {
+    if (indexPath.item == NA_BACKSPACE_BUTTON_INDEX) {
         return [[FAKIonIcons backspaceOutlineIconWithSize:25.0] characterCode];
     } else if (indexPath.item == 10) {
         return @"0";
-    } else if (indexPath.item == 9) {
-        return self.shouldShowDotKey ? @"." : @"000";
+    } else if (indexPath.item == NA_DOT_OR_THOUDSAND_BUTTON_INDEX) {
+        switch (self.type) {
+            case KVAmountInputTextFieldTypePercentage:
+            case KVAmountInputTextFieldTypeQuantity:
+                return self.numberFormatter.decimalSeparator;
+            case KVAmountInputTextFieldTypeCurrency:
+                return @"000";
+            default:
+                break;
+        }
     }
     return [NSString stringWithFormat:@"%d", (int)indexPath.item + 1];
 }
@@ -112,7 +117,7 @@
 }
 
 - (nullable SEL)numberPad:(LENumberPad *)numberPad touchDownActionForButtonAtIndexPath:(NSIndexPath *)indextPath {
-    if (indextPath.row == 11) {
+    if (indextPath.row == NA_BACKSPACE_BUTTON_INDEX) {
         return @selector(onBackSpaceButtonTouchDown:);
     }
     return nil;
@@ -124,9 +129,9 @@
 }
 
 - (void)changeValueWithButton:(UIButton *)button {
-    
     if (button.state != UIControlStateNormal ) {
-        [self numberPad:self.numberPad didSelectButtonAtIndexPath:[NSIndexPath indexPathForRow:11 inSection:0]];
+        [self numberPad:self.numberPad didSelectButtonAtIndexPath:[NSIndexPath indexPathForRow:NA_BACKSPACE_BUTTON_INDEX
+                                                                                     inSection:0]];
         CGFloat elapsed = MAX( 1, fabs( [self.start timeIntervalSinceNow]));
         CGFloat delay = 0.15 / elapsed;
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)( delay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
@@ -139,99 +144,156 @@
 #pragma mark - LENumberPadDelegate
 
 - (void)numberPad:(LENumberPad *)numberPad didSelectButtonAtIndexPath:(NSIndexPath *)indexPath {
+    NSNumber *amount = @0;
     NSString *text = self.text;
 
-    text = [text stringByReplacingOccurrencesOfString:@"," withString:@""];
+    text = [text stringByReplacingOccurrencesOfString:self.numberFormatter.groupingSeparator withString:@""];
     
-    if (indexPath.item == 11) {
+    if (indexPath.item == NA_BACKSPACE_BUTTON_INDEX) {
         if (text.length > 1) {
             text = [text substringToIndex:text.length - 1];
+            amount = [self.numberFormatter numberFromString:text];
         } else {
-            text = self.noValueDisplayText;
+            amount = nil;            
         }
-
+        self.amount = amount;
+    } else if (indexPath.item == NA_DOT_OR_THOUDSAND_BUTTON_INDEX && [self shouldShowDot]) {
+        if (![self.text containsString:self.numberFormatter.decimalSeparator]) {
+            self.text = [self.text stringByAppendingString:self.numberFormatter.decimalSeparator];
+        }        
+        return;
     } else {
-        NSString *stringValue = [self buttonTitleAtIndexPath:indexPath];
-        if ([self needAppendText:stringValue intext:text]) {
-            text = [text stringByAppendingString:stringValue];
+        UIButton *button = [numberPad buttonAtIndexPath:indexPath];
+        NSMutableString *string = [NSMutableString stringWithString:text];
+        [string appendString:button.titleLabel.text];
+        amount = [self.numberFormatter numberFromString:string];
+        if (self.type == KVAmountInputTextFieldTypePercentage) {
+            if ([amount doubleValue] > 100) {
+                amount = @100;
+            }
         }
-    }
-    
-    double amount = [[self.numberFormater numberFromString:text] doubleValue];
-    if (self.amountValue != amount) {
-        self.amountValue = amount;
-        if (self.valueChangedBlock) {
-            self.valueChangedBlock(self.amountValue, self.shouldShowDotKey);
+        if ([self shouldChangeAmount:amount]) {
+            self.amount = amount;
+            [self didChangeAmount:amount];
         }
-    } else {
-        self.text = text;
     }
 }
 
-- (BOOL)needAppendText:(NSString *)stringValue intext:(NSString *)text {
-    if (self.shouldShowDotKey) {
-        
-        if (_amountValue == 100) {
-            return NO;
-        }
-        
-        NSInteger length = text.length;
-        if ([text containsString:@"."]) {
-            NSRange range = [text rangeOfString:@"."];
-            if ([stringValue isEqualToString:@"."] || length - range.location == self.numberFormater.maximumFractionDigits + 1) {
-                return NO;
-            }
-        }
-        return _amountValue > 0
-                || [[self.numberFormater numberFromString:stringValue] boolValue]
-                || [stringValue isEqualToString:@"."]
-                || length == self.numberFormater.maximumFractionDigits
-                || length == 0;
+#pragma mark - privates
+
+- (BOOL)shouldShowDot {
+    return self.type == KVAmountInputTextFieldTypeQuantity ||
+    (self.type == KVAmountInputTextFieldTypePercentage && [self.amount doubleValue] < 100);
+}
+
+- (void)configDotOrThousandButton {
+    UIButton *button = [self.numberPad buttonAtIndexPath:[NSIndexPath indexPathForRow:NA_DOT_OR_THOUDSAND_BUTTON_INDEX
+                                                                            inSection:0]];
+    if (self.type == KVAmountInputTextFieldTypePercentage ||
+        self.type == KVAmountInputTextFieldTypeQuantity) {
+        [button setTitle:self.numberFormatter.decimalSeparator forState:UIControlStateNormal];
+        [button setTitle:self.numberFormatter.decimalSeparator forState:UIControlStateSelected];
+    } else {
+        [button setTitle:@"000" forState:UIControlStateNormal];
+        [button setTitle:@"000" forState:UIControlStateSelected];
     }
-    
-    return _amountValue > 0 || [[self.numberFormater numberFromString:stringValue] boolValue];
+}
+
+- (BOOL)shouldChangeAmount:(NSNumber *)amount {
+    // delegate is assigned
+    if (self.delegate && [self.delegate respondsToSelector:@selector(textField:shouldChangeAmount:)]) {
+        return [self.delegate textField:self shouldChangeAmount:self.amount];
+    }
+    switch (self.type) {
+        case KVAmountInputTextFieldTypeCurrency:
+            return YES;
+            break;
+        case KVAmountInputTextFieldTypeQuantity:
+            return ![self reachMaximumFractionDigitsCharacters:self.text];
+        case KVAmountInputTextFieldTypePercentage:
+            return ![self reachMaximumFractionDigitsCharacters:self.text];
+        default:
+            break;
+    }
+    return NO;
+}
+
+- (BOOL)reachMaximumFractionDigitsCharacters:(NSString *)text {
+    if ([text containsString:self.numberFormatter.decimalSeparator]) {
+        NSRange range = [text rangeOfString:self.numberFormatter.decimalSeparator];
+        if ([text isEqualToString:self.numberFormatter.decimalSeparator] ||
+            text.length - range.location < self.numberFormatter.maximumFractionDigits + 1) {
+            return NO;
+        } else {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (void)didChangeAmount:(NSNumber *)amount
+{
+    if ([delegate respondsToSelector:@selector(textField:didChangeAmount:)]) {
+        [delegate textField:self didChangeAmount:amount];
+    }
+}
+
+- (BOOL)textFieldShouldClear:(UITextField *)textField {
+    if (self.resetToZeroIfCleared) {
+        self.amount = @0;
+        return NO;
+    } else {
+        self.amount = nil;
+        if (self.delegate && [self.delegate respondsToSelector:@selector(textFieldShouldClear:)]) {
+            return [self.delegate textFieldShouldClear:textField];
+        }
+        return NO;
+    }    
 }
 
 #pragma mark - Setter
 
-- (void)setShouldShowDotKey:(BOOL)shouldShowDotKey {
-    _shouldShowDotKey = shouldShowDotKey;
-    self.numberFormater.maximumFractionDigits = self.shouldShowDotKey ? 2 : 0;
-    UIButton *button = [self.numberPad buttonAtIndexPath:[NSIndexPath indexPathForRow:9 inSection:0]];
-    [button setTitle:_shouldShowDotKey ? @"." : @"000" forState:UIControlStateNormal];
-    [button setTitle:_shouldShowDotKey ? @"." : @"000" forState:UIControlStateSelected];
-}
-
-- (void)setAmountValue:(double)amountValue {
-    if (_amountValue != amountValue) {
-        _amountValue = amountValue;
-        if (_amountValue > 0) {
-            self.text = [self.numberFormater stringFromNumber:@(_amountValue)];
-        } else {
-            self.text = self.noValueDisplayText;
-        }
+- (void)setAmount:(NSNumber *)amount {
+    if (amount) {
+        self.text = [self.numberFormatter stringFromNumber:amount];
+    } else {
+        self.text = self.resetToZeroIfCleared ? @"0" : nil;
     }
 }
 
-- (void)setNoValueDisplayText:(NSString *)noValueDisplayText {
-    _noValueDisplayText = noValueDisplayText;
-    if (_amountValue == 0) {
-        self.text = noValueDisplayText;
+- (void)setType:(KVAmountInputTextFieldType)type {
+    _type = type;
+    switch (type) {
+        case KVAmountInputTextFieldTypeCurrency:
+            _numberFormatter.maximumFractionDigits = 0;
+            break;
+        case KVAmountInputTextFieldTypeQuantity:
+            _numberFormatter.maximumFractionDigits = 3;
+            break;
+        case KVAmountInputTextFieldTypePercentage:
+             _numberFormatter.maximumFractionDigits = 2;
+            break;
+        default:
+            break;
     }
+    [self configDotOrThousandButton];
 }
 
 #pragma mark - Getter
 
-- (NSNumberFormatter *)numberFormater {
-    if (!_numberFormater) {
-        _numberFormater = [[NSNumberFormatter alloc] init];
-        _numberFormater.numberStyle = NSNumberFormatterDecimalStyle;
-        _numberFormater.roundingMode = NSNumberFormatterRoundHalfUp;
-        _numberFormater.decimalSeparator = @".";
-        _numberFormater.groupingSeparator = @",";
-        _numberFormater.maximumFractionDigits = self.shouldShowDotKey ? 2 : 0;
+- (NSNumber *)amount {
+    return [self.numberFormatter numberFromString:self.text];
+}
+
+- (NSNumberFormatter *)numberFormatter {
+    if (!_numberFormatter) {
+        _numberFormatter = [[NSNumberFormatter alloc] init];
+        _numberFormatter.numberStyle = NSNumberFormatterDecimalStyle;
+        _numberFormatter.roundingMode = kCFNumberFormatterRoundUp;
+        _numberFormatter.decimalSeparator = @".";
+        _numberFormatter.groupingSeparator = @",";
     }
-    return _numberFormater;
+    return _numberFormatter;
 }
 
 - (LENumberPad *)numberPad {
@@ -245,14 +307,5 @@
     }
     return _numberPad;
 }
-
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
 
 @end
