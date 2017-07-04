f5684950439201b2424f3d4cf96e26202779d826
anh.dn <anh.dn@citigo.net>
2017-07-04 11:47:18 +0700

    chore(LEAmountInputViewDemoTests): update test case

--- LEAmountInputViewDemoTests/LEAmountInputViewDemoTests.m
+++ LEAmountInputViewDemoTests/LEAmountInputViewDemoTests.m
@@ -45,7 +45,7 @@
                                   @"1,234,567,890,123.000" : @(NO),
                                   @"1,234,567,890,123.001" : @(NO)};
     for (NSString *key in testStrings.allKeys) {
-        BOOL result = [key isInvalidStringForNumberFormatter:self.textField.numberFormatter];
+        BOOL result = [key isInvalidStringForNumberFormatter:self.textField.numberFormatter maxLenght:13];
         XCTAssertEqual((int)result, [[testStrings valueForKey:key] integerValue] , @"%@", key);
     }
 }
@@ -64,7 +64,7 @@
                                   @"" : @(YES),
                                   @"1,234,567,890,123.001" : @(YES)};
     for (NSString *key in testStrings.allKeys) {
-        BOOL result = [key isInvalidStringForNumberFormatter:self.textField.numberFormatter];
+        BOOL result = [key isInvalidStringForNumberFormatter:self.textField.numberFormatter maxLenght:13];
         XCTAssertEqual((int)result, [[testStrings valueForKey:key] integerValue] , @"%@", key);
     }
 }
@@ -79,17 +79,18 @@
                                   @"#$=" : @(YES),
                                   @"AB" : @(YES)};
     for (NSString *key in testStrings.allKeys) {
-        BOOL result = [key isInvalidStringForNumberFormatter:self.textField.numberFormatter];
+        BOOL result = [key isInvalidStringForNumberFormatter:self.textField.numberFormatter maxLenght:3];
         XCTAssertEqual((int)result, [[testStrings valueForKey:key] integerValue] , @"%@", key);
     }
 }
 
-- (void)testAmounTextField {
+- (void)testAmountTextField {
     
     self.textField.type = KVAmountInputTextFieldTypeQuantity;
     XCTAssertEqual( [self.textField.numberFormatter numberFromString:@"9,999,999,999,999.999"].doubleValue, 9999999999999.999 , @"%@", self.textField.amount);
     NSDictionary *testStrings = @{@"9,999,999,999,999.999" : [NSNumber numberWithDouble:9999999999999.999],
-                                  @"999,999,999,999.999" :  [NSNumber numberWithDouble:999999999999.999]};
+                                  @"999,999,999,999.999" :  [NSNumber numberWithDouble:999999999999.999],
+                                  @"9999999999999.999" :  [NSNumber numberWithDouble:9999999999999.999]};
     for (NSString *key in testStrings.allKeys) {
         XCTAssertEqual( [self.textField.numberFormatter numberFromString:key].doubleValue, [[testStrings valueForKey:key] doubleValue] , @"%@", self.textField.amount);
     }
@@ -100,7 +101,9 @@
     NSDecimalNumberHandler *round = [NSDecimalNumberHandler decimalNumberHandlerWithRoundingMode:NSRoundPlain scale:3 raiseOnExactness:NO raiseOnOverflow:YES raiseOnUnderflow:YES raiseOnDivideByZero:NO];
     NSDecimalNumber *decimalNumber = [NSDecimalNumber decimalNumberWithString:[@"9,999,999,999,999.9999" stringByReplacingOccurrencesOfString:@"," withString:@""]];
     NSNumber *number = (NSNumber *)[decimalNumber decimalNumberByRoundingAccordingToBehavior:round];
-    XCTAssertEqual(number.doubleValue, 9999999999999.999);
+//    XCTAssertEqual(number.doubleValue, 9999999999999.999);
+    XCTAssertEqual(number.doubleValue, 10000000000000);
     
 }
+
 @end
